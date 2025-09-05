cf8086445735fa1f345877d3ae1019a9131e3555
Lamberto Nunez <132211239+Benover75@users.noreply.github.com>
2025-09-05 18:59:03 -0400

    Delete weather-mlops-pipeline directory

--- weather-mlops-pipeline/test/test_model.py
+++
@@ -1,6 +0,0 @@
-# tests/test_model.py
-def test_model_file():
-    from weather_mlops_pipeline.train.train_model import train
-    train()
-    import os
-    assert os.path.exists("data/models/weather_model.pkl")
